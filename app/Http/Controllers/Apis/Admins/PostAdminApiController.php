812c634db36cf20ac81f1267d4571cad8733b6eb
gibihu <nawapol0010@gmail.com>
2025-09-25 17:35:54 +0700

    แก้บัก a | A

---
+++ app/Http/Controllers/Apis/Admins/PostAdminApiController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Apis\Admins;
+use App\Http\Controllers\Controller;
+
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+
+use App\Models\User;
+use App\Models\Post;
+
+class PostAdminApiController extends Controller
+{
+    public function show(Request $request) {
+        try{
+            $data = Post::with('user')->orderBy('created_at', 'desc')->get();
+            $data->transform(function($item) {
+                $item->title_short = Str::limit($item->title, 40, '...');
+                return $item;
+            });
+            return response()->json([
+                'message' => 'สำเร็จ',
+                'data' => $data,
+                'code' => 200
+            ], 200);
+        }catch (\Exception $e) {
+            $response = [
+                'message' => 'มีบางอย่างผิดพลาด โปรดลองอีกครั้งในภายหลัง',
+                'code' => 500,
+            ];
+            if(env('APP_DEBUG')) $response['debug'] = [
+                'message' => $e->getMessage(),
+                'request' => $request->all(),
+            ];
+            return response()->json($response, 500);
+        }
+    }
+}
---
+++ app/Http/Controllers/Apis/Admins/WalletAdminApiController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers\Apis\Admins;
+use App\Http\Controllers\Controller;
+
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+
+use App\Models\User;
+use App\Models\Wallet;
+use App\Models\WalletHistory;
+
+class WalletAdminApiController extends Controller
+{
+    public function show(Request $request) {
+        try{
+            $history = WalletHistory::with('wallet.user')->where('role', WalletHistory::ROLE_ADD)->orderBy('created_at', 'desc')->get();
+            $history->transform(function($item) {
+                return [
+                    'id' => $item->id,
+                    'wallet_id' => $item->wallet_id,
+                    'user' => [
+                        'id' => $item->wallet->user->id,
+                        'name' => $item->wallet->user->name,
+                        'username' => $item->wallet->user->username,
+                        'email' => $item->wallet->user->email,
+                    ],
+                    'change_amount' => $item->change_amount,
+                    'role' => $item->role,
+                    'role_text' => $item->role_text,
+                    'type' => $item->type,
+                    'type_text' => $item->type_text,
+                    'description' => $item->description,
+                    'created_at' => $item->created_at,
+                    'updated_at' => $item->updated_at,
+                ];
+            });
+            return response()->json([
+                'message' => 'สำเร็จ',
+                'data' => $history,
+                'code' => 200
+            ], 200);
+        }catch (\Exception $e) {
+            $response = [
+                'message' => 'มีบางอย่างผิดพลาด โปรดลองอีกครั้งในภายหลัง',
+                'code' => 500,
+            ];
+            if(env('APP_DEBUG')) $response['debug'] = [
+                'message' => $e->getMessage(),
+                'request' => $request->all(),
+            ];
+            return response()->json($response, 500);
+        }
+    }
+}
