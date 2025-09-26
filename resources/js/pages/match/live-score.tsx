7515cf2981e70115da3ea861046f491162e46f09
gibihu <nawapol0010@gmail.com>
2025-09-26 21:05:20 +0700

    update live api 2

--- resources/js/pages/match/live-score.tsx
+++ resources/js/pages/match/live-score.tsx
@@ -11,15 +11,15 @@ import { BoardTable } from "./board-table";
 const API_URL: string = import.meta.env.VITE_API_URL;
 export function LiveScore() {
     const [matches, setMatches] = useState<MatchType[]>([]);
-    const [isFetchBoard, setIsFetchBoard] = useState(false);
+    const [isloading, setIsloading] = useState(true);
     const [leagues, setLeagues] = useState<CompetitionType[]>([]);
     const [filters, setFilters] = useState<(CompetitionType & { matches: MatchType[] })[]>([]);
 
     const [isMatchFetch, setIsMatchFetch] = useState<boolean>(true);
+    const [isLeagueFetch, setIsLeagueFetch] = useState<boolean>(false);
 
     useEffect(() => {
         const fetchData = async () => {
-            setIsFetchBoard(true);
             setIsMatchFetch(true);
             // const res = await fetch(`https://livescore-api.com/api-client/matches/live.json?&key=O9GiRG3laCyROLBr&secret=tsL0gvXuGlkKJUgx4XQVEUhPwPHlBiM5&lang=en`);
             const res = await fetch(api.match.live().url);
@@ -33,7 +33,6 @@ export function LiveScore() {
                 const errors = result;
                 toast.error(result.message);
             }
-            setIsFetchBoard(false);
             setIsMatchFetch(false);
         };
         fetchData();
@@ -42,31 +41,10 @@ export function LiveScore() {
         // ล้าง interval เมื่อ component ถูก unmount
         return () => clearInterval(intervalId);
     }, []);
-    useEffect(() => {
-        const fetchData = async () => {
-            const ids = matches.map((item: MatchType) => item.competition?.id).join(",");
-            setIsFetchBoard(true);
-            // const res = await fetch(`https://livescore-api.com/api-client/matches/live.json?&key=O9GiRG3laCyROLBr&secret=tsL0gvXuGlkKJUgx4XQVEUhPwPHlBiM5&lang=en`);
-            const res = await fetch(`${api.league.all().url}?filter_ids=${ids}`);
-
-            const result = await res.json();
-            // if (result.code == 200) {
-            if (result.code == 200) {
-                const data = await result.data;
-                setLeagues(data);
-            } else {
-                const errors = result;
-                toast.error(result.message);
-            }
-            setIsFetchBoard(false);
-        };
-        if(!isMatchFetch){
-            fetchData();
-        }
-    }, [matches]);
 
     function hanffleRelod() {
         const fetchData = async () => {
+            setIsMatchFetch(true);
             // const res = await fetch(`https://livescore-api.com/api-client/matches/live.json?&key=O9GiRG3laCyROLBr&secret=tsL0gvXuGlkKJUgx4XQVEUhPwPHlBiM5&lang=en`);
             const res = await fetch(api.match.live().url);
 
@@ -79,9 +57,32 @@ export function LiveScore() {
                 const errors = result;
                 toast.error(result.message);
             }
+            setIsMatchFetch(false);
         };
         fetchData();
     }
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const ids = matches.map((item: MatchType) => item.competition?.id).join(",");
+            setIsLeagueFetch(true);
+            const res = await fetch(`${api.league.all().url}?filter_ids=${ids}`);
+
+            const result = await res.json();
+            // if (result.code == 200) {
+            if (result.code == 200) {
+                const data = await result.data;
+                setLeagues(data);
+            } else {
+                const errors = result;
+                toast.error(result.message);
+            }
+            setIsLeagueFetch(false);
+        };
+        if(!isMatchFetch){
+            fetchData();
+        }
+    }, [isMatchFetch]);
 
     useEffect(() => {
         // แสดง leagues เสมอ แม้ว่า matches จะยังไม่มีข้อมูล
@@ -98,17 +99,12 @@ export function LiveScore() {
             });
 
             setFilters(updatedFilters);
+            setIsloading(false);
         }
     }, [matches, leagues]);
 
-    useEffect(() => {
-        setTimeout(() => {
-            console.log(filters);
-        }, 1000);
-    });
-
     return (
         // <BoardScore items={matches} isFetch={isFetchBoard} />
-        <BoardTable items={filters} isFetch={isFetchBoard} />
+        <BoardTable items={filters} isFetch={isloading} />
     );
 }
